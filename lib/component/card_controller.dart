7fe4909e5a963db902231dfc6b512dc3f0d723aa
Tharinda Abeysena <matabeysena@students.nsbm.lk>
2021-08-31 20:45:46 +0530

    Add the date and add name to the place text. removed unwanted dart files. Final Application

--- lib/component/card_controller.dart
+++ lib/component/card_controller.dart
@@ -1,6 +1,4 @@
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
-class CardController extends GetxController {
-  final String date = 'Date';
-}
+class CardController extends GetxController {}
